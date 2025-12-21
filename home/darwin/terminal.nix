44206f113836e324aafb42d878774dd44ecfaac7
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-21 13:11:44 +0800

    Update terminal.nix

--- home/darwin/terminal.nix
+++ home/darwin/terminal.nix
@@ -1,8 +1,8 @@
-{ lib, ... }:
 let
-  fontSize = 15;
+  fontSize = 15;  # Define terminal font size
 in
 {
+  # Set font size for various terminal programs
   programs.alacritty.settings.font.size = lib.mkForce fontSize;
   programs.ghostty.settings.font-size = lib.mkForce fontSize;
   programs.kitty.font.size = lib.mkForce fontSize;
