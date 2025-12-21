15659ca64838effd587a4c62eff77e4611f49287
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-21 13:06:25 +0800

    Update default.nix

--- home/darwin/aerospace/default.nix
+++ home/darwin/aerospace/default.nix
@@ -1,5 +1,6 @@
 { config, ... }:
 {
+  # Link the Aerospace configuration file
   home.file.".aerospace.toml".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/home/darwin/aerospace/aerospace.toml";
 }
