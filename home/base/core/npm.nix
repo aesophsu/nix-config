9acc42028be1f86a720fc21b8d7fa9f313266fcd
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-21 12:49:17 +0800

    Refactor npm.nix to streamline configuration
    Removed comments and simplified npm configuration.

--- home/base/core/npm.nix
+++ home/base/core/npm.nix
@@ -1,9 +1,5 @@
-{ config, ... }:
 {
-  # make `npm install -g <pkg>` happey
-  #
-  # mainly used to install npm packages that updates frequently
-  # such as gemini-cli, claude-code, etc.
+  # Configure npm global install location
   home.file.".npmrc".text = ''
     prefix=${config.home.homeDirectory}/.npm
   '';
