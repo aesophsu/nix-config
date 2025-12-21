baf1810f016f051b914166117db7d44ae4de331c
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-21 12:50:19 +0800

    Refactor AWS and Google Cloud integration settings

--- home/base/core/starship.nix
+++ home/base/core/starship.nix
@@ -1,24 +1,18 @@
 {
   programs.starship = {
     enable = true;
-
     enableBashIntegration = true;
     enableZshIntegration = true;
     enableNushellIntegration = true;
 
-    # https://starship.rs/config/
     settings = {
-      # Get editor completions based on the config schema
       "$schema" = "https://starship.rs/config-schema.json";
       character = {
         success_symbol = "[➜](bold green)";
         error_symbol = "[➜](bold red)";
       };
-      # I never rely on the defaults, so this module is useless to me—disabled.
-      # I prefer adding --project, --region to very gcloud/aws command.
-      aws.disabled = true;
-      gcloud.disabled = true;
-
+      aws.disabled = true;  # Disable AWS integration
+      gcloud.disabled = true;  # Disable Google Cloud integration
       kubernetes = {
         symbol = "⛵";
         disabled = false;
