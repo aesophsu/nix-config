2df002374d614138c57c5a420e72698126c812b9
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-21 12:44:01 +0800

    Update home.nix

--- home/base/home.nix
+++ home/base/home.nix
@@ -1,18 +1,9 @@
 { myvars, ... }:
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home = {
     inherit (myvars) username;
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "24.11";
+    # Defines the Home Manager state version to ensure compatibility with the release
+    stateVersion = "24.11";  # Update if needed to match current Home Manager release
   };
 }
