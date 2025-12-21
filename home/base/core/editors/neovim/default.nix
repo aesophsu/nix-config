2f105c70de6ca5b33b0ec01d72b8ee78695bc99f
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-21 12:53:42 +0800

    Refactor Neovim configuration syntax

--- home/base/core/editors/neovim/default.nix
+++ home/base/core/editors/neovim/default.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 {
-  programs = {
-    neovim = {
-      enable = true;
-
-      viAlias = true;
-      vimAlias = true;
-    };
+  programs.neovim = {
+    enable = true;
+    viAlias = true;  # Enable `vi` alias for Neovim
+    vimAlias = true;  # Enable `vim` alias for Neovim
   };
 }
