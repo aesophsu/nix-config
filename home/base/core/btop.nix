b4aeb760224d22c7c1bcb89e7ec6b3ebe0f5eeda
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-21 12:45:01 +0800

    Refactor comment formatting in btop.nix

--- home/base/core/btop.nix
+++ home/base/core/btop.nix
@@ -1,9 +1,8 @@
 {
-  # replacement of htop/nmon
   programs.btop = {
     enable = true;
     settings = {
-      theme_background = false; # make btop transparent
+      theme_background = false;  # Makes btop transparent
     };
   };
 }
