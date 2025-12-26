5e2fc6c88c787e380b2dca233a982f61b53ed571
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 22:45:19 +0800

    Delete home/base/core/pip.nix

--- home/base/core/pip.nix
+++
@@ -1,8 +0,0 @@
-{
-  # Use a mirror for pip installs to speed up package downloads
-  xdg.configFile."pip/pip.conf".text = ''
-    [global]
-    index-url = https://mirror.nju.edu.cn/pypi/web/simple
-    format = columns
-  '';
-}
