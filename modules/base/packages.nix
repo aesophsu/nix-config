8c61436d9af8dbb9dcbfd2aacf9e008930e2e7a3
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 20:16:45 +0800

    Update packages.nix

--- modules/base/packages.nix
+++ modules/base/packages.nix
@@ -12,79 +12,8 @@
   # =====================================================================================
 
   environment.systemPackages = with pkgs; [
-
-    # Shell & core tools
-    nushell
-    # fastfetch
-    neovim
-    # gnumake
-    # just
     git
     git-lfs
-
-    # Monitoring
-    # procs
-    # btop
-
-    # Archives
-    # zip
-    # xz
-    # zstd
-    # unzipNLS
-    # p7zip
-
-    # Text & data processing
-    # gnugrep
-    # gawk
-    # gnutar
-    # gnused
-    # sad
-    # jq
-    # yq-go
-    # jc
-
-    # Search & navigation
-    # fzf
-    # fd
-    # findutils
-    # (ripgrep.override { withPCRE2 = true; })
-
-    # Disk usage
-    # duf
-    # dust
-    # gdu
-    # ncdu
-
-    # Networking & diagnostics
-    # mtr
-    # gping
-    # dnsutils
-    # ldns
-    # doggo
-    # wget
-    # curl
-    # curlie
-    # httpie
-    # aria2
-    # socat
-    # nmap
-    # ipcalc
-    # iperf3
-    # hyperfine
-    # tcpdump
-
-    # File transfer
-    # rsync
-    # croc
-
-    # Security & crypto
-    # libargon2
     openssl
-
-    # Misc
-    # file
-    # which
-    # tree
-    # tealdeer
   ];
 }
