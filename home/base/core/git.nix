64f12a3b8d3ea2b18d722dea36434347dfe3cbc0
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 22:00:16 +0800

    Update git.nix

--- home/base/core/git.nix
+++ home/base/core/git.nix
@@ -1,72 +1,23 @@
+{ myvars, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  myvars,
-  ...
-}:
-{
-  home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-    rm -f ${config.home.homeDirectory}/.gitconfig
-  '';
-
-  programs.gh = {
-    enable = true;
-    settings = {
-      git_protocol = "ssh";
-      prompt = "enabled";  # Enable GitHub CLI prompts
-      aliases = {
-        co = "pr checkout";
-        pv = "pr view";
-      };
-    };
-    hosts = {
-      "github.com" = {
-        "users" = {
-          "aesophsu" = null;
-        };
-        "user" = "aesophsu";
-      };
-    };
-  };
-
   programs.git = {
     enable = true;
     lfs.enable = true;
+
+    userName = myvars.userfullname;
+    userEmail = myvars.useremail;
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+    };
+
     includes = [
       {
         path = "~/work/.gitconfig";
         condition = "gitdir:~/work/";
       }
     ];
-
-    settings = {
-      user.email = myvars.useremail;
-      user.name = myvars.userfullname;
-      init.defaultBranch = "main";
-      trim.bases = "develop,master,main";  # For git-trim
-      push.autoSetupRemote = true;
-      pull.rebase = true;
-      log.date = "iso";  # ISO date format for logs
-      url = {
-        "ssh://git@github.com/aesophsu" = {
-          insteadOf = "https://github.com/aesophsu";
-        };
-      };
-    };
   };
-
-  programs.delta = {
-    enable = true;
-    enableGitIntegration = true;
-    options = {
-      diff-so-fancy = true;
-      line-numbers = true;
-      true-color = "always";
-    };
-  };
-
-  programs.lazygit.enable = true;
-
-  programs.gitui.enable = false;
 }
