7809b647e27a14a0d0f7f1286d2cc46f3623a7e6
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 20:08:49 +0800

    Delete home/darwin/aerospace directory

--- home/darwin/aerospace/default.nix
+++
@@ -1,6 +0,0 @@
-{ config, ... }:
-{
-  # Link the Aerospace configuration file
-  home.file.".aerospace.toml".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/home/darwin/aerospace/aerospace.toml";
-}
