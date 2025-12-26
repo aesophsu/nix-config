58aac91c784704380a3c0465084b59107c58d42a
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 20:09:20 +0800

    Delete home/darwin/rime-squirrel.nix

--- home/darwin/rime-squirrel.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  # Configure Squirrel Input Method with custom data
-  home.file."Library/Rime" = {
-    source = "${pkgs.flypy-squirrel}/share/rime-data";  # Custom Squirrel data for Flypy input method
-    recursive = true;
-    force = true;  # Overwrite dynamically generated data if necessary
-  };
-}
