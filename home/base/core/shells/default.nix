5275d13b8f7c926ade266e69af6a53d8633abeca
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 22:55:07 +0800

    Update default.nix

--- home/base/core/shells/default.nix
+++ home/base/core/shells/default.nix
@@ -1,15 +1,11 @@
 { config, lib, ... }: 
 let
   shellAliases = {
-    k = "kubectl";  # Alias for kubectl
     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";  # Decode URL
     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";  # Encode URL
   };
 
   localBin = "${config.home.homeDirectory}/.local/bin";
-  goBin = "${config.home.homeDirectory}/go/bin";
-  rustBin = "${config.home.homeDirectory}/.cargo/bin";
-  npmBin = "${config.home.homeDirectory}/.npm/bin";
 in
 {
   # Bash setup with additional directories in PATH
