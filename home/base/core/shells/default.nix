b552deca35342577906b7e751533897e04b668b7
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 22:01:44 +0800

    Update default.nix

--- home/base/core/shells/default.nix
+++ home/base/core/shells/default.nix
@@ -1,23 +1,18 @@
-{ config, lib, ... }: 
+{ config, ... }: 
 let
-  # Shell aliases for URL encoding and decoding
-  shellAliases = {
-    urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";  # Decode URL
-    urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";  # Encode URL
-  };
-
-  localBin = "${config.home.homeDirectory}/.local/bin";  # Local bin path (optional if no other bins are required)
+  localBin = "${config.home.homeDirectory}/.local/bin";
 in
 {
-  # Zsh setup with additional shell aliases
-  programs.zsh = {
+  programs.bash = {
     enable = true;
     enableCompletion = true;
-    zshrcExtra = ''
+    bashrcExtra = ''
       export PATH="$PATH:${localBin}"
     '';
   };
 
-  # Shell aliases available in Zsh
-  home.shellAliases = shellAliases;
+  home.shellAliases = {
+    ".." = "cd ..";
+    "..." = "cd ../..";
+  };
 }
