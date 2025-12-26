c25b736cb7f1050d237d11dfe693ca135e2cd3d1
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-26 22:09:29 +0800

    Update shell.nix

--- home/darwin/shell.nix
+++ home/darwin/shell.nix
@@ -1,28 +1,23 @@
 { lib, ... }:
 let
-  # Additional environment variables to add to PATH
   envExtra = ''
-    export PATH="$PATH:/opt/homebrew/bin:/usr/local/bin"
+    export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
   '';
-  # Conda initialization based on architecture
+
   initContent = ''
-    arch=$(uname -m)
-    if [ "aarch64" = "$arch" ] || [ "arm64" = "$arch" ]; then
+    if [ "$(uname -m)" = "arm64" ]; then
       if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
         . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
       else
         export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
       fi
-    elif [[ "x86_64" = "$arch" ]]; then
-      true  # Do nothing for x86_64
     fi
   '';
 in
 {
-  # Enable Zsh with specific environment configurations
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    zshrcExtra = lib.mkAfter (envExtra + initContent);  # Combine environment variables and Conda initialization
+    zshrcExtra = lib.mkAfter (envExtra + initContent);
   };
 }
