61f3ca2ce27d31e39b7cfa44165350137418d6a1
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-28 17:14:06 +0800

    Update system.nix

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,83 +1,38 @@
-{
-  pkgs,
-  myvars,
-  ...
-}:
+{ pkgs, myvars, ... }:
 
 {
-  # Enable TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  # Set system time zone
   time.timeZone = "Asia/Shanghai";
 
   system = {
     primaryUser = myvars.username;
 
     defaults = {
-      # Dock settings
       dock = {
         autohide = false;
         show-recents = false;
         mru-spaces = false;
-        expose-group-apps = true;
       };
 
-      # Finder settings
       finder = {
         _FXShowPosixPathInTitle = true;
-        AppleShowAllExtensions = false;
+        AppleShowAllExtensions = true;
         FXEnableExtensionChangeWarning = false;
-        FXDefaultSearchScope = "SCcf";
-        QuitMenuItem = true;
         ShowPathbar = true;
         ShowStatusBar = true;
       };
 
-      # Trackpad settings
-      trackpad = {
-        Clicking = true;
-        TrackpadRightClick = true;
-        TrackpadThreeFingerDrag = true;
-      };
+      trackpad.Clicking = true;
 
-      # Global macOS settings
       NSGlobalDomain = {
-        "com.apple.swipescrolldirection" = true;
-        "com.apple.sound.beep.feedback" = 0;
         AppleInterfaceStyle = "Dark";
         InitialKeyRepeat = 15;
         KeyRepeat = 3;
         ApplePressAndHoldEnabled = false;
-        NSAutomaticCapitalizationEnabled = false;
-        NSAutomaticSpellingCorrectionEnabled = false;
-        NSDocumentSaveNewDocumentsToCloud = false;
-      };
-
-      # Custom user preferences
-      CustomUserPreferences = {
-        ".GlobalPreferences" = {
-          AppleSpacesSwitchOnActivate = true;
-        };
-        "com.apple.finder" = {
-          AppleShowAllFiles = false;
-          ShowExternalHardDrivesOnDesktop = true;
-          ShowHardDrivesOnDesktop = false;
-        };
-        "com.apple.screensaver" = {
-          askForPassword = 1;
-          askForPasswordDelay = 0;
-        };
-      };
-
-      # Login window settings
-      loginwindow = {
-        GuestEnabled = false;
-        SHOWFULLNAME = true;
       };
     };
 
-    # Keyboard settings for remapping keys
     keyboard = {
       enableKeyMapping = true;
       remapCapsLockToEscape = false;
