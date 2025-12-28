c1ab5a26b39cf9c8e0c6445bdea0196c2736e67c
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-28 17:15:28 +0800

    Update nix-core.nix

--- modules/darwin/nix-core.nix
+++ modules/darwin/nix-core.nix
@@ -1,17 +1,12 @@
 { config, ... }:
 
 {
-  # Disable conflicting Determinate Nix installation
   nix.enable = false;
 
-  # Disable auto-store optimization to prevent linking errors
   nix.settings.auto-optimise-store = false;
   nix.gc.automatic = false;
 
   system.stateVersion = 5;
 
-  # Include secrets access tokens for additional configuration
-  nix.extraOptions = ''
-    !include ${config.age.secrets.nix-access-tokens.path}
-  '';
+  nix.extraOptions = ""; 
 }
