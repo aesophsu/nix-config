bdfaad606f728517fc81f80c7217e863262dd89d
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-28 16:57:05 +0800

    Update shell.nix

--- home/darwin/shell.nix
+++ home/darwin/shell.nix
@@ -3,21 +3,22 @@ let
   envExtra = ''
     export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
   '';
-
+  
   initContent = ''
-    if [ "$(uname -m)" = "arm64" ]; then
-      if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
+    if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
         . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
-      else
-        export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
-      fi
     fi
   '';
 in
 {
+  programs.bash = {
+    enable = true;
+    bashrcExtra = lib.mkAfter (envExtra + initContent);
+  };
+
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    zshrcExtra = lib.mkAfter (envExtra + initContent);
+    initExtra = lib.mkAfter (envExtra + initContent);
   };
+  
 }
