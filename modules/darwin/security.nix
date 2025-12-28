8c6aa7412ffd9ea35fee5ee8f7f7c1c71f213777
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-12-28 17:14:42 +0800

    Update security.nix

--- modules/darwin/security.nix
+++ modules/darwin/security.nix
@@ -1,28 +1,16 @@
-{
-  config,
-  myvars,
-  ...
-}:
+{ config, myvars, ... }:
 
 let
   homeDir = config.users.users."${myvars.username}".home;
 in
 {
-  # Enable GPG agent with logging configuration
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = false;
   };
 
-  # Set logs for GPG agent debugging
   launchd.user.agents.gnupg-agent.serviceConfig = {
     StandardErrorPath = "${homeDir}/Library/Logs/gnupg-agent.stderr.log";
     StandardOutPath = "${homeDir}/Library/Logs/gnupg-agent.stdout.log";
   };
-
-  # Disable password authentication for SSH
-  environment.etc."ssh/sshd_config.d/200-disable-password-auth.conf".text = ''
-    PasswordAuthentication no
-    KbdInteractiveAuthentication no
-  '';
 }
